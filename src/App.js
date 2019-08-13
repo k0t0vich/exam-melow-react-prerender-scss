a1b8a3d2ccb518399894039a7e4911582efffbbb
Konstantin Bondarenko <k.bondarenko@corp.mail.ru>
2019-08-13 12:19:34 +0300

    refactoring: move canvas resize logic to Melow from App

--- src/App.js
+++ src/App.js
@@ -12,35 +12,14 @@ import './App.scss';
 export default class App extends Component {
     constructor(props){
       super(props)
-      this.state = {
-          width:0,
-          height:0,
-      }
-      this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-
-    componentDidMount() {
-      this.updateWindowDimensions();
-      window.addEventListener('resize', this.updateWindowDimensions);
-      console.log(store.currentWeight);
-    }
-
-    componentWillUnmount() {
-      window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions() {
-      this.setState({ width: window.innerWidth, height: window.innerHeight });
     }
 
     render() {
         return (
             <>
                 <TitleHeader/>
-                <TaskHeader weight={store.currentWeight}/>
+                <TaskHeader store={store}/>
                 <Melow 
-                  width={this.state.width} 
-                  height={this.state.height} 
                   innerRadius={store.innerRadius} 
                   outerRadius={store.outerRadius}
                 />
--- src/components/Melow.js
+++ src/components/Melow.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 import '../App.scss';
 export default class Melow extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            width:0,
+            height:0,
+            scale: 1
+        }
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
     componentDidMount() {
-        this.updateCanvas();
+      this.updateWindowDimensions();
+      this.updateCanvas();
+      window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        const canvas = document.getElementById("canvas");
+        const offsetTop = canvas ? canvas.offsetTop: 0;
+        const relativeHeight = window.innerHeight - offsetTop; //TODO add delta for (offsetBottom?)
+        const relativeWidth = window.innerWidth; //TODO add delta for (offsetLeft?)
+        const maxRadius = Math.min(relativeHeight, relativeWidth) * 0.5 * 0.7;
+        console.log("canvas " + canvas);
+        
+        this.setState({ 
+            width: relativeWidth, 
+            height: relativeHeight,
+            scale: maxRadius / this.props.outerRadius
+        });
     }
     
     componentDidUpdate() {
@@ -10,13 +41,14 @@ export default class Melow extends Component {
     }
 
     updateCanvas() {
+        console.log("updateCanvas");
         const ctx = this.refs.canvas.getContext('2d');
-        const cx = this.props.width/2;
-        const cy = this.relativeHeight/2;
+        const cx = this.state.width/2;
+        const cy = this.state.height/2;
         const PI2 = Math.PI * 2;
         
         ctx.beginPath();
-        ctx.arc(cx, cy, this.props.outerRadius * this.scaleRadius, 0, PI2, true); 
+        ctx.arc(cx, cy, this.props.outerRadius * this.state.scale, 0, PI2, true); 
         ctx.fillStyle = 'green';
         ctx.fill();
         ctx.stroke();
@@ -24,7 +56,7 @@ export default class Melow extends Component {
 
         ctx.moveTo(cx, cy);
         ctx.beginPath();
-        ctx.arc(cx, cy, this.props.innerRadius * this.scaleRadius, 0, PI2, true); 
+        ctx.arc(cx, cy, this.props.innerRadius * this.state.scale, 0, PI2, true); 
         ctx.fillStyle = 'red';
         ctx.fill();
         ctx.stroke();
@@ -32,13 +64,8 @@ export default class Melow extends Component {
     }
 
     render() {
-        const canvas = document.getElementById("canvas");
-        const offsetTop = canvas ? canvas.offsetTop: 0;
-        this.relativeHeight = this.props.height - offsetTop;
-        const maxRadius = Math.min(this.relativeHeight, this.props.width) * 0.5 * 0.7;
-        this.scaleRadius = maxRadius / this.props.outerRadius;
         return (
-            <canvas id="canvas" ref="canvas" width={this.props.width} height={this.relativeHeight}/>
+            <canvas id="canvas" ref="canvas" width={this.state.width} height={this.state.height}/>
         );
     }
 }
